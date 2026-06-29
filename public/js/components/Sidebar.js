1515fee487be70d0e14a47a8aee57fcd5f2c812f
zaimcherosli <huzaimrosli@gmail.com>
2026-06-29 20:47:27 +0800

    Redirect Sidebar Schedule link to fully functional calendar.html

--- public/js/components/Sidebar.js
+++ public/js/components/Sidebar.js
@@ -59,7 +59,7 @@ class Sidebar extends HTMLElement {
                             </a>
                         </li>
                         <li>
-                            <a href="schedule.html" class="nav-item" data-route="schedule">
+                            <a href="calendar.html" class="nav-item" data-route="calendar">
                                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
                                     <line x1="16" y1="2" x2="16" y2="6" />

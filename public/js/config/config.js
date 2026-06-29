e45e1d8b78c061830ff9febf2707698ed195d716
zaimcherosli <huzaimrosli@gmail.com>
2026-06-29 12:49:36 +0800

    Update API_BASE_URL to point to correct custom domain api.socialhub.zaimrosli.my

--- public/js/config/config.js
+++ public/js/config/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:8787/api'
         : (window.location.origin.includes('zaimrosli.my')
-            ? 'https://socialhub-worker.zaimrosli.my/api'
+            ? 'https://api.socialhub.zaimrosli.my/api'
             : '/api'),
     PLATFORMS: {
         THREADS: 'threads',

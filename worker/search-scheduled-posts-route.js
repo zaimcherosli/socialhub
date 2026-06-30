5eb52c6565a08ba3ed8dc0bbdfa3d85e853d383a
zaimcherosli <huzaimrosli@gmail.com>
2026-07-01 02:48:40 +0800

    clean: Remove search scripts

--- worker/search-scheduled-posts-route.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/index.js', 'utf8');
-const lines = content.split('\n');
-lines.forEach((line, idx) => {
-    if (line.includes('scheduled-posts') || line.includes('scheduled_posts')) {
-        console.log(`${idx + 1}: ${line.trim()}`);
-    }
-});

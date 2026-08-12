d550ed0255b76d5f14db8dea4459085e77d7b7f3
zaimcherosli <huzaimrosli@gmail.com>
2026-08-12 23:05:34 +0800

    v1.4.86: Version bump and UI refinement for AI quota system

--- public/js/config.js
+++ public/js/config.js
@@ -4,10 +4,10 @@
  */
 (function() {
     const CONFIG = Object.freeze({
-        VERSION: '1.4.85',
+        VERSION: '1.4.86',
         BUILD_DATE: new Date().toLocaleDateString('ms-MY'),
         ENV: 'production',
-        PWA_VERSION: 'v1.4.85'
+        PWA_VERSION: 'v1.4.86'
     });
 
     window.SYS_CONFIG = CONFIG;

583445dfcdb4374477e1259bab355dea0d8482c5
Zaim Rosli <huzaimrosli@gmail.com>
2026-08-29 07:39:07 +0800

    chore: bump version to v1.9.2

--- public/js/config.js
+++ public/js/config.js
@@ -4,10 +4,10 @@
  */
 (function() {
     const CONFIG = Object.freeze({
-        VERSION: '1.9.1',
+        VERSION: '1.9.2',
         BUILD_DATE: new Date().toLocaleDateString('ms-MY'),
         ENV: 'production',
-        PWA_VERSION: 'v1.9.1'
+        PWA_VERSION: 'v1.9.2'
     });
 
     window.SYS_CONFIG = CONFIG;

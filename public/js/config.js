a1d98d3a6b0b6a1eae53334321aabd3ce38cbbbd
Zaim Rosli <huzaimrosli@gmail.com>
2026-09-05 11:18:43 +0800

    feat(autopilot): auto-sync daily frequency and campaign length with dynamic options and live summary badge

--- public/js/config.js
+++ public/js/config.js
@@ -4,10 +4,10 @@
  */
 (function() {
     const CONFIG = Object.freeze({
-        VERSION: '2.0.0',
+        VERSION: '2.0.1',
         BUILD_DATE: new Date().toLocaleDateString('ms-MY'),
         ENV: 'production',
-        PWA_VERSION: 'v2.0.0'
+        PWA_VERSION: 'v2.0.1'
     });
 
     window.SYS_CONFIG = CONFIG;
